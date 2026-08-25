1a14be9f80fe9915bc478880a3cbb7f35b84d72a
Navya <tnavyasrireddy@gmail.com>
2026-08-25 22:13:39 +0530

    Implement threeSumClosest function in Solution class
    using two pointer left and right 
    similar to 3 sum problem 
    but here we need to return the closest sum to the target
    total=nums[i]+nums[left]+nums[right]
    total =closest
    return closest

---
+++ 3sumclosest.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        closest=nums[0]+nums[1]+nums[2]
+        for i in range(len(nums)-2):
+            left=i+1
+            right=len(nums)-1
+            while left<right:
+                total=nums[i]+nums[left]+nums[right]
+                if total==target:
+                    return total
+                if abs(total-target)<abs(closest-target):
+                    closest=total
+                if total<target:
+                    left+=1
+                else:
+                    right-=1
+        return closest
